cf3de923c70361666597a6fd4f988a567094a624
gsh <18320850996@139.com>
2019-12-01 23:55:24 +0800

    指定样式

---
+++ pages/02-text/02-text.js
@@ -0,0 +1,72 @@
+// pages/02-text/02-text.js
+Page({
+
+  /**
+   * 页面的初始数据
+   * RN -> state,Vue -> data
+   */
+  data: {
+    title1: '我是一个文本...\n',
+    title2: '我是二个文本...\n',
+    title3: '我是三个文本...\n',
+    title4: '我是四个文本...\n',
+    isShow: false,
+    num: 5
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
